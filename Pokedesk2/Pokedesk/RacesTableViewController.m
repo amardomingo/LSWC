585129fabf1cf1cb531d8fdf389078cbe106dde9
Álvaro Martín Rodríguez <alvaro.martin.rodriguez@alumnos.upm.es>
2014-04-08 20:09:07 +0200

    Pokedex added SearchBar

--- Pokedesk2/Pokedesk/RacesTableViewController.m
+++ Pokedesk2/Pokedesk/RacesTableViewController.m
@@ -11,8 +11,8 @@
 #import "Type.h"
 #import "Race.h"
 
-@interface RacesTableViewController ()
-
+@interface RacesTableViewController () <UISearchBarDelegate,UISearchDisplayDelegate>
+@property (strong, nonatomic) NSArray * filteredRacesArray;
 @end
 
 @implementation RacesTableViewController
@@ -20,6 +20,14 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    self.filteredRacesArray = [NSArray array];
+    
+    // Registramos como se crean las celdas de la
+    // tabla de resultados.
+    [self.searchDisplayController.searchResultsTableView registerClass: [UITableViewCell class]
+                                                forCellReuseIdentifier: @"PokemonCell"];
+    self.searchDisplayController.searchResultsTableView.separatorStyle = UITableViewCellSeparatorStyleNone;
+    
 }
 
 #pragma mark - Table view data source
@@ -31,7 +39,11 @@
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return [self.type.races count];
+    if(tableView == self.searchDisplayController.searchResultsTableView) {
+        return [self.filteredRacesArray count];
+    }else {
+        return [self.type.races count];
+    }
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
@@ -39,7 +51,12 @@
     static NSString *CellIdentifier = @"PokemonCell";
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
     
-    Race * race = self.type.races[indexPath.row];
+    Race * race;
+    if(tableView == self.searchDisplayController.searchResultsTableView) {
+        race = self.filteredRacesArray[indexPath.row];
+    } else {
+        race = self.type.races[indexPath.row];
+    }
     
     cell.imageView.image = [UIImage imageNamed:race.icon];
     cell.textLabel.text = race.name;
@@ -47,12 +64,32 @@
     return cell;
 }
 
+- (void) updateFilteredRacesWithName: (NSString * ) text {
+    NSPredicate *pred = [NSPredicate predicateWithFormat:@"SELF.name contains[c] %@" , text];
+    self.filteredRacesArray = [self.type.races filteredArrayUsingPredicate:pred];
+}
+
+-(BOOL) searchDisplayController:(UISearchDisplayController *)controller shouldReloadTableForSearchString:(NSString *)searchString{
+    [self updateFilteredRacesWithName:searchString];
+    return YES;
+}
+
+//segue programaticamente
+-(void) tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    [self performSegueWithIdentifier:@"Show Wiki Info"sender:tableView];
+}
+
 - (void) prepareForSegue: (UIStoryboardSegue *)segue sender:(id)sender
 {
     if ([segue.identifier isEqualToString:@"Show Wiki Info"]) {
-        NSIndexPath *ip = [self.tableView indexPathForCell:sender];
         WebWikiViewController *wvc = segue.destinationViewController;
-        wvc.race = self.type.races[ip.item];
+        if(sender == self.searchDisplayController.searchResultsTableView){
+            NSIndexPath *ip = [self.searchDisplayController.searchResultsTableView indexPathForSelectedRow];
+            wvc.race = self.filteredRacesArray[ip.row];
+        } else {
+            NSIndexPath *ip = [self.tableView indexPathForSelectedRow];
+            wvc.race = self.type.races[ip.row];
+        }        
     }
 }
 
